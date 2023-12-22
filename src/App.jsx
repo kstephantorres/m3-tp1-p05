da174ff77f49a44eaec838023737f7f7dcfc6949
K Stephan Torres <kevinstephantorres@gmail.com>
2023-12-22 01:49:02 -0300

    Agregamos el maquetado del componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
 import './App.css'
+import {Container} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from './components/Footer'
 
 function App() {
 
-  return (
+  return ( 
     <>
-      
-    </>
+      <Container className='py-4 '>
+        <h1 className='text-center text-white'>Lista de tareas </h1>
+      </Container> 
+      <Footer></Footer>
+    </>     
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-light text-center py-3">
+            <p>Todos los derechos reservados &copy;</p>
+        </footer>
+    )
+}
+
+export default Footer;
