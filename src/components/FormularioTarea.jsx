d69fb364e8ace4a76221544f2c6b5b498eec5604
K Stephan Torres <kevinstephantorres@gmail.com>
2023-12-27 10:17:13 -0300

    Agregamos las tareas en el estado listaTarea

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,17 +1,35 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTarea from './ListaTarea'
-
+import { useState } from "react";
 
 const FormularioTarea = () => {
+    const [tarea, setTarea]= useState('');
+    const [listaTarea, setListaTarea] = useState([])
+
+    // handle+algo nombre de funciones para el manejo de states 
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        // realizar algo similar al push tarea
+        // operador spred ...
+        setListaTarea([...listaTarea,tarea.trim()])
+    }
+
     return (
         <section>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
-                    <Form.Control type="text" placeholder="Ej: Tarea 1" minLength={3} maxLength={80} />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Ej: Tarea 1" 
+                        minLength={3} 
+                        maxLength={80} 
+                        onChange={(e)=> setTarea(e.target.value)} 
+                        value={tarea}
+                    />
                     <Button variant="dark" className="ms-2" type="submit"> Agregar </Button>
-                </Form.Group>
-                <ListaTarea></ListaTarea>
+                </Form.Group> 
             </Form>
+            <ListaTarea></ListaTarea>
         </section>
     );
 };
